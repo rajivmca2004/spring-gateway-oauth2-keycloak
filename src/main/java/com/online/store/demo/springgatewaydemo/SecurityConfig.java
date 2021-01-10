3a49d419e2e317623c8e818fae87143a00932a61
Rajiv Srivastava <rajivmca2004@gmail.com>
2021-01-11 00:10:19 +0530

    Refactor

--- src/main/java/com/online/store/demo/springgatewaydemo/SecurityConfig.java
+++ src/main/java/com/online/store/demo/springgatewaydemo/SecurityConfig.java
@@ -1,14 +1,13 @@
 package com.online.store.demo.springgatewaydemo;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 
-@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 	
